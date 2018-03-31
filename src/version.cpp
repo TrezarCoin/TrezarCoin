b19af4aa2c75c72ce01f8223c41bb642aaa0760d
TrezarCoin <31046773+TrezarCoin@users.noreply.github.com>
2018-03-31 16:40:32 +0200

    Minor fix
    orb to tzc

--- src/version.cpp
+++ src/version.cpp
@@ -41,7 +41,7 @@ const std::string CLIENT_NAME("Trezarcoin");
 #endif
 
 #define BUILD_DESC_FROM_COMMIT(maj,min,rev,build,commit) \
-    "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) "-orb" commit
+    "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) "-tzc" commit
 
 #define BUILD_DESC_FROM_UNKNOWN(maj,min,rev,build) \
     "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build)
