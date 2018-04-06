bf29e7df560d70332ba1fed6ac572a36829cb8cf
Peter Bushnell <bushsolo@gmail.com>
2018-04-06 08:24:47 +0100

    watchonly > iswatchonly

--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -1663,7 +1663,7 @@ Value validateaddress(const Array& params, bool fHelp)
         isminetype mine = pwalletMain ? IsMine(*pwalletMain, dest) : MINE_NO;
         ret.push_back(Pair("ismine", mine != MINE_NO));
         if (mine != MINE_NO) {
-            ret.push_back(Pair("watchonly", mine == MINE_WATCH_ONLY));
+            ret.push_back(Pair("iswatchonly", mine == MINE_WATCH_ONLY));
             Object detail = boost::apply_visitor(DescribeAddressVisitor(mine), dest);
             ret.insert(ret.end(), detail.begin(), detail.end());
         }
@@ -1701,7 +1701,7 @@ Value validatepubkey(const Array& params, bool fHelp)
         ret.push_back(Pair("ismine", mine != MINE_NO));
         ret.push_back(Pair("iscompressed", isCompressed));
         if (mine != MINE_NO) {
-            ret.push_back(Pair("watchonly", mine == MINE_WATCH_ONLY));
+            ret.push_back(Pair("iswatchonly", mine == MINE_WATCH_ONLY));
             Object detail = boost::apply_visitor(DescribeAddressVisitor(mine), dest);
             ret.insert(ret.end(), detail.begin(), detail.end());
         }
