553583966a98a5f9c46e97883a745e3c2b0c417d
Peter Bushnell <bushsolo@gmail.com>
2018-09-24 09:57:28 +0100

    conf, pid and datadir

--- src/util.cpp
+++ src/util.cpp
@@ -99,8 +99,8 @@ namespace boost {
 
 using namespace std;
 
-const char * const BITCOIN_CONF_FILENAME = "bitcoin.conf";
-const char * const BITCOIN_PID_FILENAME = "bitcoind.pid";
+const char * const BITCOIN_CONF_FILENAME = "trezarcoin.conf";
+const char * const BITCOIN_PID_FILENAME = "trezarcoind.pid";
 
 map<string, string> mapArgs;
 map<string, vector<string> > mapMultiArgs;
@@ -468,7 +468,7 @@ boost::filesystem::path GetDefaultDataDir()
     // Unix: ~/.bitcoin
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "Bitcoin";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "TrezarCoin";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -478,10 +478,10 @@ boost::filesystem::path GetDefaultDataDir()
         pathRet = fs::path(pszHome);
 #ifdef MAC_OSX
     // Mac
-    return pathRet / "Library/Application Support/Bitcoin";
+    return pathRet / "Library/Application Support/trezarcoin";
 #else
     // Unix
-    return pathRet / ".bitcoin";
+    return pathRet / ".trezarcoin";
 #endif
 #endif
 }
