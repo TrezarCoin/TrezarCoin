49724ef3a43da94cf1c277c6dd97590138510dd7
Peter Bushnell <bushsolo@gmail.com>
2018-09-29 06:26:54 +0100

    Testnet coinbase confs

--- src/main.cpp
+++ src/main.cpp
@@ -2850,7 +2850,6 @@ void InitTestnet() {
     nModifierIntervalOne = 60;
     /* [Current] interval of 30 seconds between stake modifiers */
     nModifierIntervalTwo = 30;
-    nBaseMaturity = BASE_MATURITY_TESTNET;
 }
 
 bool static LoadBlockIndexDB()
