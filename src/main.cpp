55b84d004f472e01fe1a77e6e2036d5fe371407a
Peter Bushnell <bushsolo@gmail.com>
2018-11-01 07:39:26 +0000

    Skip genesis on CB height check

--- src/main.cpp
+++ src/main.cpp
@@ -3749,9 +3749,10 @@ bool ContextualCheckBlock(const CBlock& block, CValidationState& state, CBlockIn
         }
     }
 
-    CScript expect = CScript() << nHeight;
-    if (!std::equal(expect.begin(), expect.end(), block.vtx[0].vin[0].scriptSig.begin())) {
-        return state.DoS(100, false, REJECT_INVALID, "bad-cb-height", false, "block height mismatch in coinbase");
+    if (nHeight > 0) {
+        CScript expect = CScript() << nHeight;
+        if (!std::equal(expect.begin(), expect.end(), block.vtx[0].vin[0].scriptSig.begin()))
+            return state.DoS(100, false, REJECT_INVALID, "bad-cb-height", false, "block height mismatch in coinbase");
     }
 
     // Validation for witness commitments.
